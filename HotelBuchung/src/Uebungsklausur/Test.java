77dc4425967511a2a0bf87789323ea46804e7cbd
Jonasm22 <john.2114@hotmail.com>
2024-07-14 20:37:50 +0200

    Hotel Reservation Program Version 1.0

---
+++ HotelBuchung/src/Uebungsklausur/Test.java
@@ -0,0 +1,20 @@
+package Uebungsklausur;
+
+public class Test {
+
+	
+	
+	public static int Calculo(int a, int b) {
+		
+		return a * b;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println(Calculo(4,4));
+	}
+
+}
